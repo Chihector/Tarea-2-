10a90c15868ad66f5ef87e5ba135b5ba342a4414
FrancoAv1 <48526545+FrancoAv1@users.noreply.github.com>
2023-10-10 19:16:21 -0300

    Update ColoniaHormigas.py

--- ColoniaHormigas.py
+++ ColoniaHormigas.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 import numpy as np
 
+# Función para calcular la matriz de distancias entre ciudades
 def obtenerDistancias(numVar,matriz):
     matrizDistancias = np.full((numVar,numVar), fill_value=-1.0, dtype=float)
 
@@ -12,18 +13,21 @@ def obtenerDistancias(numVar,matriz):
             matrizDistancias[j][i] = distancia
 
     return matrizDistancias
-
+    
+# Función para calcular la matriz de heurísticas
 def obtenerHeuristicas(matriz):
     matrizHeuristicas = np.full_like(matriz, fill_value=(1/matriz), dtype=float)
     np.fill_diagonal(matrizHeuristicas, 0.0)
 
     return matrizHeuristicas
 
+# Función para inicializar la matriz de feromonas
 def obtenerFeromonas(matriz,numVar,mejorCosto):
     matrizFeromonas = np.full_like(matriz, 1/(mejorCosto*numVar))
 
     return matrizFeromonas
 
+# Función para calcular el costo de una solución
 def solucionCalcularCosto(cantNodos,matrizSolucion,matrizBase):
     costo = matrizBase[matrizSolucion[cantNodos-1]][matrizSolucion[0]]
     for i in range(cantNodos-1):
@@ -31,6 +35,7 @@ def solucionCalcularCosto(cantNodos,matrizSolucion,matrizBase):
 
     return costo
 
+# Función para inicializar hormigas en posiciones aleatorias
 def inicializarHormigas(hormiga, nodos):
     poblacion = np.full((hormiga, nodos), -1)
     for i in range(hormiga):
@@ -38,6 +43,7 @@ def inicializarHormigas(hormiga, nodos):
 
     return poblacion
 
+# Función para seleccionar el siguiente segmento de la ruta
 def seleccionarNuevoSegmento(nodos,tamañoPobl,pobl,feromona,feromLocal,probLim,matrizDistancias,valorHeur,factorEvapFerom):
     matrizNodos = np.arange(nodos)
     for i in range(tamañoPobl):
@@ -69,6 +75,7 @@ def seleccionarNuevoSegmento(nodos,tamañoPobl,pobl,feromona,feromLocal,probLim,
             
     return pobl
 
+# Comprobar que se proporcionan los argumentos esperados
 if len(sys.argv) == 8:
     semilla = int(sys.argv[1])
     matrizCoordenadas = str(sys.argv[2])
@@ -88,25 +95,35 @@ else:
     print('Error en la entrada de los parametros')
     sys.exit(0)
 
+# Fijar la semilla aleatoria
 np.random.seed(semilla)
 
+# Leer las coordenadas de las ciudades desde el archivo
 archivo = pd.read_csv("burma14.txt", header=None, skiprows=6, skipfooter=1, engine='python', delim_whitespace=True, usecols=(1,2), dtype=int)
 matrizArchivo  = archivo.to_numpy()
 numVariables = matrizArchivo.shape[0]
 matrizDistancias = obtenerDistancias(numVariables,matrizArchivo)
 matrizHeuristicas = obtenerHeuristicas(matrizDistancias)
+
+# Inicializar una solución inicial aleatoria y calcular su costo
 mejorSol = np.arange(0,numVariables)
 np.random.shuffle(mejorSol)
 solucionMejorCosto = solucionCalcularCosto(numVariables,mejorSol,matrizDistancias)
+
+# Inicializar la matriz de feromonas con valores iniciales
 feromona = obtenerFeromonas(matrizDistancias,numVariables,solucionMejorCosto)
 feromonaLocal = 1/(solucionMejorCosto*numVariables)
 
+# Inicio del ciclo principal de iteraciones
 while numIt > 0 and not np.round(solucionMejorCosto,decimals=4) == 7544.3659:
+    # Inicio del ciclo principal de iteraciones
     poblacion = inicializarHormigas(tamañoPob, numVariables)
 
+     # Realizar la selección de segmentos para cada hormiga
     for i in range(numVariables-1):
         poblacion = seleccionarNuevoSegmento(numVariables,tamañoPob,poblacion,feromona,feromonaLocal,probLimite,matrizHeuristicas,Heuristica,factEvapFeromona)
 
+    # Evaluar el costo de cada solución generada por las hormigas
     for i in range(tamañoPob):
         aux = solucionCalcularCosto(numVariables,poblacion[i][:],matrizDistancias)
         if aux < solucionMejorCosto:
@@ -114,6 +131,7 @@ while numIt > 0 and not np.round(solucionMejorCosto,decimals=4) == 7544.3659:
             mejorSolucion = poblacion[i][:]
             print("En la iteración: ", numIt, " se encontró una mejor solución. Esta es: ", solucionMejorCosto)
 
+    # Evaluar el costo de cada solución generada por las hormigas
     for i in range(numVariables):
         for j in range(numVariables):
             feromona[i][j] = (1-factEvapFeromona)*feromona[i][j]
@@ -125,8 +143,9 @@ while numIt > 0 and not np.round(solucionMejorCosto,decimals=4) == 7544.3659:
     
     feromona[mejorSolucion[0]][mejorSolucion[-1]] = (1-factEvapFeromona)*feromona[mejorSolucion[0]][mejorSolucion[-1]] + factEvapFeromona/solucionMejorCosto
     feromona[mejorSolucion[-1]][mejorSolucion[0]] = feromona[mejorSolucion[0]][mejorSolucion[-1]]
+    # Información de la iteración
 
     print("Iteración: ",numIt)
     numIt -= 1
-
+#Mostrar mejor solución encontrada
 print("Costo mejor solución: ", np.round(solucionMejorCosto,decimals=4), "\nSolución: ", mejorSolucion)
